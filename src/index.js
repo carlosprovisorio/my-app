e3d221db901ca48f1da1c785c182eb8fde127d04
Carlos Alberto Ferreira <carlos@Carloss-MacBook-Air.local>
2022-03-21 22:39:09 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create a react component
+const App = () => {
+    return (
+        <div>
+            <label class="label" for="name"> 
+                Enter Name: 
+            </label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                Submit
+            </button>
+
+        </div>
+    );
+};
+    
+
+// Take the react component and show it on the screen 
+ReactDOM.render ( <App />, document.querySelector('#root'));
